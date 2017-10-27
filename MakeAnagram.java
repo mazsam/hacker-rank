4d788c9552304980f0f7dc9b732e94d2372d69c5
mazsam <arifinsam.sam@gmail.com>
2017-10-27 16:20:06 +0700

    Solution hacker rank make anagram

---
+++ MakeAnagram.java
@@ -0,0 +1,27 @@
+public class MakeAnagram{
+
+    public static void main (String[] args){
+        String a = "cde";
+        String b = "abc";        
+
+        System.out.println(Solution(a,b));
+    }
+
+    public static int Solution(String a, String b){
+        int[] counts = new int[20];
+        for(char i : a.toCharArray()){
+            counts[i-'a']++;
+        }
+
+        for(char i : b.toCharArray()){
+            counts[i-'a']--;
+        }
+
+        int num = 0;
+
+        for(int i : counts){
+            num += Math.abs(i);
+        }
+        return num;
+    }
+}
